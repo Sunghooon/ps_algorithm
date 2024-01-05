98702b389ce7e97cccf866a61dce6ca083f3fc01
hoonie <hooniebeenie@naver.com>
2024-01-05 15:25:58 +0900

    lv1 ps practice

---
+++ lv_1/add_two_num.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int T;
+    cin >> T;
+
+    vector<int> answer(T); // dynamically assign 
+    int a, b; // 1 <= a,b <= 9
+    for(int t = 0; t < T; t++)
+    {
+        cin >> a >> b;
+        answer[t] = a+b;
+    }
+    for(int t = 0; t < T; t++)
+    {
+         cout << "Case #" << t+1 << ": " << answer[t] << endl;   
+    }
+    
+   return 0;
+}
